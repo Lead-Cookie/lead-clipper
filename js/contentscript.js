947f72e84892be2a3bbc499039a58d4f3564b2cf
Ashish Belwase <belwasetech@Ashishs-MacBook-Pro.local>
2018-09-14 11:17:35 +0545

    old ui title/company

--- js/contentscript.js
+++ js/contentscript.js
@@ -1,35 +1,12 @@
 
 function scrapeNewUI(){
 
-  //var dt_classes = $('dt.result-lockup__name');
-
   var dt_classes = $('div.result-lockup__entity');
   return parseNewLeadElement(dt_classes);
-
-  // var lead_elements = dt_classes.filter(function() { return $(this).find('a[id*="ember"]').length > 0});
-  // if(lead_elements.length == 1) {
-  //   return [parseNewLeadElement(lead_elements)]
-  // } else {
-  //   return dt_classes.filter(function() { return $(this).find('a[id*="ember"]').length > 0}).map(function() {
-  //     return parseNewLeadElement(this)
-  //   })
-  // }
 
 }
 
 function parseNewLeadElement(dt_classes) {
-  // var salesUrl = 'https://www.linkedin.com' + $(lead_element).find('a[id*="ember"]').attr('href').split('?')[0];
-  
-  // var stringArray = $(lead_element).text(); //.split(/[^A-Za-z \.']/).slice(0, 1);
-  // var fullName = stringArray.trim();
-  // var firstName = fullName.split(' ').slice(0, 1).join(' ');
-  // var lastName = fullName.split(' ').slice(-1).join(' ');
-  
-  // //var salesUrl = 'https://www.linkedin.com' + salesUrl;
-  // //console.log(salesUrl)
-
-  // return {firstName: firstName, lastName: lastName, salesUrl: salesUrl}
-
 
   var results = [];
   for(i=0;i<dt_classes.length;i++ ) {
@@ -85,31 +62,59 @@ function parseNewLeadElement(dt_classes) {
 
 function scrapeOldUI(){
 
-  var lead_elements = $('.name').filter(function() { return $(this).find('a.name-link').length > 0});
-
-  if(lead_elements.length == 1) {
-    return [parseOldLeadElement(lead_elements)]
-  } else {
-    return $('.name').filter(function() { return $(this).find('a.name-link').length > 0}).map(function() {
-      return parseOldLeadElement(this)
-    })
-  }
+  var dt_classes = $('.name-and-badge-container');
+  return parseOldLeadElement(dt_classes)
 
 }
 
 
 
-function parseOldLeadElement(lead_element) {
-  var salesUrl = $(lead_element).find('a.name-link').attr('href').split('?')[0];
-  
-  var stringArray = $(lead_element).text().split(/[^A-Za-z \.']/).slice(0, 1);
-  var fullName = stringArray[0].trim();
-  var firstName = fullName.split(' ').slice(0, 1).join(' ');
-  var lastName = fullName.split(' ').slice(-1).join(' ');
-  
-  var salesUrl = 'https://www.linkedin.com' + salesUrl;
+function parseOldLeadElement(dt_classes) {
 
-  return {firstName: firstName, lastName: lastName, salesUrl: salesUrl}
+  var results = [];
+  for(i=0;i<dt_classes.length;i++ ) {
+    var result = {firstName: '', lastName: '', salesUrl: '', currentTitle: '', currentCompany: ''};
+
+    var item = $(dt_classes[i]);
+
+    try{
+      var salesUrl =  item.find('a.name-link').attr('href').split('?')[0];
+      var stringArray = item.text().split(/[^A-Za-z \.']/).slice(0, 1);
+      var fullName = stringArray[0].trim();
+      result.firstName = fullName.split(' ').slice(0, 1).join(' ');
+      result.lastName = fullName.split(' ').slice(-1).join(' ');
+
+    }catch(err){
+      continue
+    }
+    
+    result.salesUrl = 'https://www.linkedin.com' + salesUrl;
+
+    var title = item.find('.details-container abbr');
+    if (title){
+      try{
+        result.currentTitle = $(title[0]).attr('title');
+      }catch(err){
+
+      }
+    }
+
+    var company = item.find('.details-container a');
+    if (company){
+      try{
+        result.currentCompany = $(company[0]).attr('title');
+      }catch(err){
+
+      }
+    }
+
+
+    results.push(result)
+
+
+  }
+
+  return results;
 }
 
 
@@ -131,24 +136,6 @@ function scrapeSalesNavigatorSearchResults() {
   return leads;
   
 }
-
-
-
-
-
-// function parseOldLeadElement(lead_element) {
-//   var salesUrl = $(lead_element).find('a.name-link').attr('href').split('?')[0];
-  
-//   var stringArray = $(lead_element).text().split(/[^A-Za-z \.']/).slice(0, 1);
-//   var fullName = stringArray[0].trim();
-//   var firstName = fullName.split(' ').slice(0, 1).join(' ');
-//   var lastName = fullName.split(' ').slice(-1).join(' ');
-  
-//   var salesUrl = 'https://www.linkedin.com' + salesUrl;
-
-//   return {firstName: firstName, lastName: lastName, salesUrl: salesUrl}
-// }
-
 
 
 
